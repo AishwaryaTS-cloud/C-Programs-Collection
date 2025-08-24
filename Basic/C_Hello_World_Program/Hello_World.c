beceec3b028ee03f18fa5043005bbebd3ae08364
Aishwarya T S <aishwaryatsgowda02@gmail.com>
2025-08-24 15:36:49 +0530

    uploding hello world program

---
+++ Basic/C_Hello_World_Program/Hello_World.c
@@ -0,0 +1,18 @@
+// Header file for input-output functions
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main() {
+    // Print statement to display "Hello World"
+    printf("Hello World");
+
+    return 0;  // Indicates successful program termination
+}
+
+
+// To compile and run this program in terminal (PowerShell):
+// cd D:\Programs\C-Programs-Collection\Basic\C_Hello_World_Program
+// gcc Hello_World.c
+// ./a.exe
+// Output: Hello World
+
